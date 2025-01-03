6622e5844fd88edb4fc905a31f95e7d47e9a2e4a
Mikey191 <desertigorb@gmail.com>
2025-01-03 12:05:12 +0300

    Numeric decimal

--- main.py
+++ main.py
@@ -1,6 +1,1 @@
-transactions = [
-    {"amount": 500, "type": "income"},
-    {"amount": 200, "type": "expense"},
-    {"amount": 300, "type": "income"},
-    {"amount": 100, "type": "expense"},
-]
+
